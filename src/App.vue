494143b6e82780f6f560d0c4fccb637f8450dc87
Minho Yoo <ymh0951@naver.com>
2024-02-19 20:46:48 +0900

    app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export defualt defineComponent({
-
+export default defineComponent({
+  data() {
+    return {
+      message: 0
+    }
+  },
 })
 </script>
+
+<template>
+  <p>{{ message }}</p>
+</template>
